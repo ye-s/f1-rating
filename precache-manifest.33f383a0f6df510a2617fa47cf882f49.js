78cc9ecb4cf43cc39095ec7fc08384e7915e2aff
Yevgeniy <jeka@pop-os.localdomain>
2020-09-01 21:35:27 +0300

    Updates

---
+++ precache-manifest.33f383a0f6df510a2617fa47cf882f49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39cf0f55e0f46f7a24b51e1a523356ed",
+    "url": "/f1-rating/index.html"
+  },
+  {
+    "revision": "097df577e62e8ce19fbb",
+    "url": "/f1-rating/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75672eef9fd319ea7fa7",
+    "url": "/f1-rating/static/js/2.13755ea9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/f1-rating/static/js/2.13755ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097df577e62e8ce19fbb",
+    "url": "/f1-rating/static/js/main.7d548fd4.chunk.js"
+  },
+  {
+    "revision": "1761d07d3ed3fbdf0634",
+    "url": "/f1-rating/static/js/runtime-main.811ab897.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/f1-rating/static/media/logo.5d5d9eef.svg"
+  }
+]);
